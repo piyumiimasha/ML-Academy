f2e131312e3d7315ce9c51fc8b749b8272df9887
piyumiima <piyumiimasha9@gmail.com>
2025-09-02 21:52:05 +0530

    add model evaluation

--- Week 05 - pipeline/src/model_evaluation.py
+++ Week 05 - pipeline/src/model_evaluation.py
@@ -18,3 +18,36 @@ from sklearn.metrics import (
 warnings.filterwarnings('ignore')
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
+
+class ModelEvaluator:
+    def __init__(
+        self,
+        model,
+        model_name
+    ):
+        self.model = model
+        self.model_name = model_name
+        self.evaluation_results = {}
+
+    def evaluate(
+            self,
+            X_test,
+            y_test
+    ):
+        Y_pred = self.model.predict(X_test)
+
+        cm = confusion_matrix(y_test, Y_pred)
+        accuracy = accuracy_score(y_test, Y_pred)
+        precision = precision_score(y_test, Y_pred)
+        recall = recall_score(y_test, Y_pred)
+        f1 = f1_score(y_test, Y_pred)
+
+        self.evaluation_results = {
+            "confusion_matrix": cm,
+            "accuracy": accuracy,
+            "precision": precision,
+            "recall": recall,
+            "f1": f1
+        }
+
+        return self.evaluation_results
